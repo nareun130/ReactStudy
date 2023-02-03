e7072e8cf0c28826083520415356b6e3c5763714
nareun130 <nareun130@gmail.com>
2023-02-03 17:52:31 +0900

    포켓몬 도감
    json활용해서 포켓몬 도감 구현

---
+++ pokemon/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// import App from './components/App';
+import App_pokemon from './App_pokemon';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App_pokemon />);
