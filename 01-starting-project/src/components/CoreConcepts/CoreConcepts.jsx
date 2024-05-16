33993f3e190b7426130eef4ad32f7707826a080d
김성호 <nareun130@gmail.com>
2024-05-16 17:56:20 +0900

    feat : React의 state, props 연습

---
+++ 01-starting-project/src/components/CoreConcepts/CoreConcepts.jsx
@@ -0,0 +1,14 @@
+import "./CoreConcepts.css";
+export default function CoreConcept({
+  image,
+  title,
+  description = "prop 기본값",
+}) {
+  return (
+    <li>
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+  );
+}
