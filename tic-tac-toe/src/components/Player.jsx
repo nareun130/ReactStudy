84cfb5741709ff703d8c0f4ca28600d3201f6a43
김성호 <nareun130@gmail.com>
2024-05-17 18:02:06 +0900

    feat : state끌어오기를 이용한 tic-tac-toe

---
+++ tic-tac-toe/src/components/Player.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+export default function Player({ initialName, symbol, isActive }) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIstEditing] = useState(false);
+
+  const handleEditClick = () => {
+    setIstEditing((editing) => !editing);
+  };
+
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+  // let btnCaption = 'Edit';
+  if (isEditing) {
+    editablePlayerName = (
+      <input
+        type="text"
+        required
+        value={playerName}
+        onChange={handleChange}></input>
+    );
+    // btnCaption = 'Save'
+  }
+  return (
+    <li className={isActive ? "active" : undefined}>
+      <span className="player">
+        {editablePlayerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
+    </li>
+  );
+}
